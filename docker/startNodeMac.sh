ceb700388b0c62c75e90326dec79746a9911e409
Yashas Salankimatt <yashas.salankimatt@gmail.com>
2024-04-19 11:12:21 -0500

    Made docker config work and almost work with unity

--- docker/startNodeMac.sh
+++ docker/startNodeMac.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker run -it \
+--user ros \
 --privileged \
---network=host \
---ipc=host \
+-p 10000:10000 \
 -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 --env DISPLAY=host.docker.internal:0 \
 thesis_image bash
