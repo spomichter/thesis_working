7aadcbb28f4bd405cd2bb1bcb0d7f14e19275b0e
Yashas Salankimatt <yashas.salankimatt@gmail.com>
2024-08-12 06:23:16 -0700

    Got VNC to work with prev thesis dev image

--- docker/startNodeLinux.sh
+++ docker/startNodeLinux.sh
@@ -6,6 +6,6 @@ docker run -it \
 --gpus all \
 --privileged \
 -p 10000:10000 \
--v /tmp/.X11-unix:/tmp/.X11-unix:rw \
---env DISPLAY=host.docker.internal:0 \
+-p 5901:5901 \
+-p 6901:6901 \
 thesis_image bash
