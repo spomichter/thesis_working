2daee8dd66eb61ffb0b57b4fa622c89be2e8fe76
Yashas Salankimatt <yashas.salankimatt@gmail.com>
2024-08-12 14:22:53 -0700

    Got setup working with ros and gui on windows

--- docker/startDockerWindows.ps1
+++ docker/startDockerWindows.ps1
@@ -1,7 +1,1 @@
-docker run -it `
---user ros `
---privileged `
--p 10000:10000 `
--v /tmp/.X11-unix:/tmp/.X11-unix:rw `
---env DISPLAY=host.docker.internal:0 `
-thesis_image bash
+docker run -it --user ros --gpus all --privileged -p 10000:10000 -p 5901:5901 -p 6901:6901 thesis_image bash
