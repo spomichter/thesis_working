4905997c09729bd2ecf6268a22b2d79b7362f4f3
Yashas Salankimatt <yashas.salankimatt@gmail.com>
2025-02-04 23:41:54 -0800

    Added shell launch file for genesis image

---
+++ docker/startGenesisLinux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+xhost +local:root
+
+docker run -dt \
+--user ros \
+--name dim_genesis_cont \
+--runtime=nvidia \
+--gpus all \
+--privileged \
+--network host \
+-p 10000:10000 \
+-p 5901:5901 \
+-p 6901:6901 \
+-e DISPLAY=$DISPLAY \
+-v /dev/dri:/dev/dri \
+-v /tmp/.X11-unix/:/tmp/.X11-unix \
+-v $PWD:/workspace \
+genesis_ros bash
